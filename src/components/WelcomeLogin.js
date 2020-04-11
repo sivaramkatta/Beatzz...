541bc969a4bd17e10a547c6ac7166d7b88d35076
sivaram004 <katta.sivaram@aasaanjobs.com>
2020-04-11 15:38:36 +0530

    added login url code

--- src/components/WelcomeLogin.js
+++ src/components/WelcomeLogin.js
@@ -1,7 +1,17 @@
 import React from "react";
 import logo from "../images/logo1.png";
 
+const scopes = "user-read-private user-read-email";
+const redirect_uri = "http://localhost:3000/dashboard/";
+const my_client_id = "ce89838d1d684c2b99207918a9062d3a";
+
 function Welcome() {
+  const RedirectToLogin = () => {
+    const loginURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${my_client_id}&redirect_uri=${encodeURIComponent(
+      redirect_uri
+    )}${scopes ? `&scope=${encodeURIComponent(scopes)}` : ""}`;
+    window.location.href = loginURL;
+  };
   return (
     <div style={styles.Container}>
       <header style={styles.AppHeader}>
@@ -9,7 +19,7 @@ function Welcome() {
         <p style={styles.Name}>
           <b>Beatzz...</b>
         </p>
-        <div style={styles.ButtonContainer}>
+        <div style={styles.ButtonContainer} onClick={RedirectToLogin}>
           <p style={styles.LoginLink}>
             <b>Login</b>
           </p>
