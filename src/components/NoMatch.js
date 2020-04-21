8741b9c833816783b22928e0fc89f6dceab79e87
sivaram004 <katta.sivaram@aasaanjobs.com>
2020-04-21 21:07:02 +0530

    added 404 pages

---
+++ src/components/NoMatch.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Hidden from "@material-ui/core/Hidden";
+import PageNotFound from "../images/pageNotFound.png";
+
+export default function NoMatch() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "10%",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <Hidden smDown>
+        <img
+          src={PageNotFound}
+          alt="img"
+          style={{ height: 300, width: 500, borderRadius: 4 }}
+        />
+      </Hidden>
+      <Hidden smUp>
+        <img
+          src={PageNotFound}
+          alt="img"
+          style={{ height: 150, width: 250, borderRadius: 4, marginTop: "20%" }}
+        />
+      </Hidden>
+      <h1>Page not found </h1>
+    </div>
+  );
+}
